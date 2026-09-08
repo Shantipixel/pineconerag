e6ba087014892924d6d3dc4bb6d957481f72df12
Shantipixel <c.shantiprabha@gmail.com>
2026-09-08 18:49:24 +0530

    Add files via upload

--- readingvectors.py
+++ readingvectors.py
@@ -5,7 +5,7 @@ import json
 
 from pinecone import Pinecone
 
-PINECONE_API_KEY = PINECONE_API_KEY
+# PINECONE_API_KEY = PINECONE_API_KEY
 
 SECTION_NUMBER_PATTERN = re.compile(r"^(\d+)(?:\.(\d+))?\.?\s+\S")
 
